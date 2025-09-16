1e6a4d94227f77137961141bf744dd89982765db
damouzo <qp241615@qmul.ac.uk>
2025-09-16 08:35:38 +0100

    Update compareBCIwithGEvar

--- bin/compareBCIwithGEvar.R
+++ bin/compareBCIwithGEvar.R
@@ -3,9 +3,9 @@
 
 # Arguments --------------------------------------------------------------------
 args <- commandArgs(trailingOnly = TRUE)
-bci_file <- args[1]  # BCI small variants RDS file
-ge_file <- args[2]    # GE small variants RDS file
-gene_name <- args[3]  # Gene name
+bci_file    <- args[1]  # "/data/BCI-KRP/projects/RR771-DHX34/results/DDX41/DDX41_bci_patients_variants.rds"
+ge_file     <- args[2]  # "/data/BCI-KRP/projects/RR771-DHX34/results/DDX41/DDX41_small_variants.rds"
+gene_name   <- args[3]  # "DDX41"
 
 
 # Libraries  -------------------------------------------------------------------
@@ -22,22 +22,21 @@ ge_variant_table <- readRDS(ge_file)
 
 
 ##############
-# Script to compare BCI patients variants with GE variants for a specific gene
 ##############
 
 # Print summary ----------------------------------------------------------------
 cat("\nGlimpse of table data of Variants \n")
-glimpse(compared_table)
+glimpse(compared_var)
 
 
 # Save variant table  --------------------------------------------------------------
 # TSV
 out_file_tsv <- paste0(gene_name, "compared_BCI_GE_small_variants.tsv")
-write.table(compared_table, file = out_file_tsv, sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(compared_var, file = out_file_tsv, sep = "\t", row.names = FALSE, quote = FALSE)
 
 # RDS
 out_file_rds <- paste0(gene_name, "compared_BCI_GE_small_variants.rds")
-saveRDS(compared_table, file = out_file_rds)
+saveRDS(compared_var, file = out_file_rds)
 
 # Print output messages
 cat("\nOutput files written:")
